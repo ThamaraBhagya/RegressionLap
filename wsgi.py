1ff7956127e05c804c8c0245b8d62b667f3adf77
Thamara Bhagya <thamarabhagya755@gmail.com>
2025-09-23 11:22:23 +0530

    as

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-# a tiny shim that exposes your Flask app as a WSGI callable
-from app import app   # app.py must define `app = Flask(...)`
-
-# some WSGI loaders look for `application` name, so expose both
-application = app
